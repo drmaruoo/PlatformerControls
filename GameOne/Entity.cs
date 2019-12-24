5ca73e9558d3bd20b3f2cee76fdf2d04552c6dde
Marcin Domarski <mdomarski@gmail.com>
2019-12-24 13:51:44 +0100

    first commit

---
+++ GameOne/Entity.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameOne
+{
+    internal class Entity
+    {
+        public int typeID;
+        public string name { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        private float speed = 16f;
+        private float gravity = 0.4f;
+        private float velocityX = 0f;
+        private float velocityY = 0f;
+        public Texture2D texture { get; set; }
+        public bool thisTickVerticalCollision;
+
+        public Entity(int typeID)
+        {
+            this.typeID = typeID;
+        }
+        public void setPosition(Vector2 position)
+        {
+            this.X = position.X;
+            this.Y = position.Y;
+        }
+        public Vector2 GetVector()
+        {
+            return new Vector2(X, Y);
+        }
+
+        public void Move(int direction)
+        {
+            thisTickVerticalCollision = CollidingWithTopBot(Game1.graphicsH);
+            GravityTick();
+            velocityX += (direction * speed) / 10;
+            if (velocityX > speed) { velocityX = speed; }
+            if (velocityX < -speed) { velocityX = -speed; }
+            if (!CollidingWithSide(Game1.graphicsW))
+            {
+                X += velocityX;
+            }
+            else
+            {
+                X -= velocityX;
+                velocityX = 0;
+            }
+            if (thisTickVerticalCollision)
+            {
+                velocityX *= 0.7f;
+            }
+            else
+            {
+                velocityX *= 0.9f;
+            }
+        }
+
+        private void GravityTick()
+        {
+            if (!thisTickVerticalCollision)
+            {
+                velocityY += gravity;
+            }
+            else
+            {
+                velocityY = 0;
+            }
+            Y += velocityY;
+        }
+
+        public void Jump()
+        {
+            if (thisTickVerticalCollision)
+            {
+                Y -= 2;
+                velocityY = -10f;
+            }
+        }
+
+        private bool CollidingWithSide(float w)
+        {
+            if (X > w - texture.Width / 2)
+            {
+                X -= 1;
+                return true;
+            }
+            else if (X < texture.Width / 2)
+            {
+                X += 1;
+                return true;
+            }
+            else { return false; }
+        }
+        private bool CollidingWithTopBot(float h)
+        {
+            if (Y > h - texture.Height / 2)
+            {
+                Y = h - texture.Height / 2;
+                return true;
+            }
+            else if (Y < texture.Height / 2)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
--- GameOne/Game1.cs
+++ GameOne/Game1.cs
@@ -4,78 +4,77 @@ using Microsoft.Xna.Framework.Input;
 
 namespace GameOne
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
+        Entity player = new Entity(0);
+
+        public static float graphicsW, graphicsH;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-        }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        }
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            graphicsW = graphics.PreferredBackBufferWidth;
+            graphicsH = graphics.PreferredBackBufferHeight;
+            player.setPosition(new Vector2(graphicsW / 2, graphicsH / 2));
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            player.texture = Content.Load<Texture2D>("player");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+
+
+            var kstate = Keyboard.GetState();
+            float egt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            int direction;
+            if (kstate.IsKeyDown(Keys.Left) && !kstate.IsKeyDown(Keys.Right)) { direction = -1; }
+            else if (kstate.IsKeyDown(Keys.Right) && !kstate.IsKeyDown(Keys.Left)) { direction = 1; }
+            else { direction = 0; }
+            if (kstate.IsKeyDown(Keys.Space)) { player.Jump(); }
+
+            player.Move(direction);
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.FromNonPremultiplied(10, 15, 15, 255));
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            spriteBatch.Draw
+                (
+                player.texture,
+                player.GetVector(),
+                null,
+                Color.White, 0f,
+                new Vector2(player.texture.Width / 2, player.texture.Height / 2),
+                Vector2.One, SpriteEffects.None,
+                0f
+                );
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
